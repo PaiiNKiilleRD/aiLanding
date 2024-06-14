ef4745bc2733f86fd6d8e72ae1c9afa09c30f3b5
PaiiNKiilleRD <javiervitalioperalta@gmail.com>
2024-06-13 20:24:52 -0400

    dark mode always enabled

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 import animations from '@midudev/tailwind-animations'
 module.exports = {
-	darkmode: "true",
+	darkmode: ["class"],
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		container: {
